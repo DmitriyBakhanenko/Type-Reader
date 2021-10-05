2c01bb336331043c40a7428c17eb1a71ca205dcf
DmitriyBakhanenko <bahanenko@gmail.com>
2021-10-05 15:25:13 +0300

    com2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './components/Login.component';
-//import Create from './components/Create.component';
+import Create from './components/Create.component';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Switch>
+        <Route exact path={'/'} component={Login} />
+        <Route exact path={'/login'} component={Login} />
+        <Route exact path={'/create'} component={Create} />
+      </Switch>
     </div>
   );
 }
